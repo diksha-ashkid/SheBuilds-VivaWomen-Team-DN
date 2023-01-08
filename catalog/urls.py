a4d2c1d6ce1ccdae6bedbf324b6951e1fe9a3f09
Nishtha Jain <79302868+2002nishthajain@users.noreply.github.com>
2023-01-08 13:58:19 +0530

    Update urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,6 +5,12 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('users/', views.UsersList.as_view(), name='users'),
     path('users/<int:pk>', views.UsersDetail.as_view(), name='user-detail'),
+    path('users/emergency', views.emergency, name='emergency'),
+    path('users/services', views.services, name='services'),
+    path('users/map', views.map, name='map'),
+    path('users/schemes', views.schemes, name='schemes'),
+    path('users/community', views.community, name='community'),
+
 
 ]
 
@@ -12,4 +18,3 @@ urlpatterns += [
     path('users/create/', views.UsersCreate.as_view(), name='user-create'),
     path('users/<int:pk>/delete/', views.UsersDelete.as_view(), name='user-delete')
 ]
-
